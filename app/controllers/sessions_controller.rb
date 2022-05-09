546ea78bc6206d0655ae8ea374372727d61c8c83
Rick <rickywu209@gmail.com>
2022-05-09 15:57:02 +0000

    add comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
-
+#creating new users
   def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
